0988fcc22819c062a9b5a06ec5b152abe4174ba7
Manan Chugh <mc2222@cornell.edu>
2018-05-31 00:50:17 -0400

    For testing saved model

---
+++ RNN/testing_chatbot.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+# Loading the weights and Running the session
+checkpoint = "./chatbot_weights.ckpt"
+session = tf.InteractiveSession()
+session.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.restore(session, checkpoint)
+
+
+
+# Converting the questions from strings to lists of encoding integers
+def convert_string2int(question, word2int):
+    question = clean_text(question)
+    return [word2int.get(word, word2int['<OUT>']) for word in question.split()]
+
+# Setting up the chat
+while (True):
+    question = input("You: ")
+    if question == 'Goodbye':
+        break
+    question = convert_string2int(question, questionswords2int)
+    question = question + [questionswords2int['<PAD>']] * (25 - len(question))
+    fake_batch = np.zeros((batch_size, 25))
+    fake_batch[0] = question
+    predicted_answer = session.run(test_predictions, {inputs: fake_batch, keep_prob: 0.5})[0]
+    answer = ''
+    for i in np.argmax(predicted_answer, 1):
+        if answersints2word[i] == 'i':
+            token = ' I'
+        elif answersints2word[i] == '<EOS>':
+            token = '.'
+        elif answersints2word[i] == '<OUT>':
+            token = 'out'
+        else:
+            token = ' ' + answersints2word[i]
+        answer += token
+        if token == '.':
+            break
+    print('ChatBot: ' + answer)
